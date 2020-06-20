af61686132de455ff55f0b06fc00370e997f2b23
mabdelmenam <34500151+mabdelmenam@users.noreply.github.com>
2020-06-19 23:15:35 -0400

    Setuo folder and core files Created mock plugin

---
+++ first-plugin.php
@@ -0,0 +1,32 @@
+<?php 
+/**
+ * @package FirstPlugin
+ * 
+ */
+/*
+Plugin Name: First Plugin
+Plugin URI: http://firstplugin.com/plugin
+Description: This is my first wordpress plugin.
+Version: 1.0.0
+Author: Mohamed Abdelmenam
+License: GPLv3
+Text Domain: first-plugin
+*/
+
+/*
+Wordpress Plugin Practice
+Copyright (C) 2020  name of Mohamed Abdelmenam
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
